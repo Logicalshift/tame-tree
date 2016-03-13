81ac3c3eddeb73d68ddc1b098288e09b73e124fd
Andrew Hunter <andrew@logicalshift.co.uk>
2016-03-13 13:55:18 +0000

    The TreeRef type is no longer necessary

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -3,12 +3,10 @@
 pub use self::treenode::*;
 pub use self::values::*;
 pub use self::basictree::*;
-pub use self::tree_ref::*;
 
 mod treenode;
 mod values;
 mod basictree;
-mod tree_ref;
 mod treenode_sugar;
 mod treenode_index;
 mod treenode_builder;
