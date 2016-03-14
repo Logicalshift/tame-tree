b8cb6043b1d59b2c3beb96bdf1262676847cbd54
Andrew Hunter <andrew@logicalshift.co.uk>
2016-03-14 23:17:26 +0000

    Rename some parts of the address, add an indexed tag address

--- src/tree/address.rs
+++ src/tree/address.rs
@@ -5,9 +5,12 @@ pub enum TreeAddress {
     /// Selects this node
     Here,
 
-    /// Selects a child of this node by index
-    ChildIndex(i32, Box<TreeAddress>),
+    /// Selects a child of this node by index, then selects a new address from there
+    ChildAtIndex(usize, Box<TreeAddress>),
 
-    /// Selects a child of this node by index
-    ChildName(String, Box<TreeAddress>)
+    /// Selects a child of this node by tag name, then selects a new address from there
+    ChildWithTag(String, Box<TreeAddress>),
+
+    /// Selects a child of this node by looking up the 'nth' child with the specified tag, then selects a new address from there
+    ChildWithIndexedTag(String, usize, Box<TreeAddress>)
 }
