cec98b9a617652d355914607465c5b0ed9162068
Andrew Hunter <andrew@logicalshift.co.uk>
2016-03-27 16:03:01 +0100

    Add a 'covers' function to TreeExtent to see if a relative address is covered by an extent

--- src/tree/extent.rs
+++ src/tree/extent.rs
@@ -1,3 +1,5 @@
+use super::address::*;
+
 ///
 /// An extent represents a series of nodes starting at a specified node
 ///
@@ -14,4 +16,61 @@ pub enum TreeExtent {
     ///
     /// Unlike Children, this covers the current node and its entire subtree
     SubTree
+}
+
+impl TreeExtent {
+    ///
+    /// Returns true if this extent will cover the specified address, which is relative to where the extent starts
+    ///
+    pub fn covers(&self, address: &TreeAddress) -> bool {
+        match *self {
+            TreeExtent::ThisNode => {
+                match *address {
+                    TreeAddress::Here   => true,
+                    _                   => false
+                }
+            },
+
+            TreeExtent::Children => {
+                match *address {
+                    TreeAddress::ChildAtIndex(_, ref child_address) => TreeExtent::ThisNode.covers(child_address),
+                    TreeAddress::ChildWithTag(_, ref child_address) => TreeExtent::ThisNode.covers(child_address),
+                    _                                               => false
+                }
+            },
+
+            TreeExtent::SubTree => true
+        }
+    }
+}
+
+#[cfg(test)]
+mod extent_tests {
+    use super::super::super::tree::*;
+
+    #[test]
+    fn thisnode_covers_only_here() {
+        assert!(TreeExtent::ThisNode.covers(&TreeAddress::Here));
+        assert!(!TreeExtent::ThisNode.covers(&(1.to_tree_address())));
+    }
+
+    #[test]
+    fn children_covers_only_immediate_children() {
+        assert!(TreeExtent::Children.covers(&(1.to_tree_address())));
+        assert!(TreeExtent::Children.covers(&("tag".to_tree_address())));
+
+        assert!(!TreeExtent::Children.covers(&((1, 2).to_tree_address())));
+        assert!(!TreeExtent::Children.covers(&(("tag", "othertag").to_tree_address())));
+        assert!(!TreeExtent::Children.covers(&TreeAddress::Here));
+    }
+
+    #[test]
+    fn subtree_covers_everything() {
+        assert!(TreeExtent::SubTree.covers(&(1.to_tree_address())));
+        assert!(TreeExtent::SubTree.covers(&("tag".to_tree_address())));
+
+        assert!(TreeExtent::SubTree.covers(&((1, 2).to_tree_address())));
+        assert!(TreeExtent::SubTree.covers(&(("tag", "othertag").to_tree_address())));
+        assert!(TreeExtent::SubTree.covers(&TreeAddress::Here));
+    }
 }
