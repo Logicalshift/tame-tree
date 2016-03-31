d4668c26a2aba30fbb3d1f1eac051d01c8352fbd
Andrew Hunter <andrew@logicalshift.co.uk>
2016-03-31 16:38:06 +0100

    Update the docs for the main lib.rs file to give a better idea of what this is about

--- src/lib.rs
+++ src/lib.rs
@@ -14,37 +14,76 @@
 //   limitations under the License.
 //
 
-//! # TreeSync
+//! # TameTree
 //! 
-//! TreeSync is a library for synchronising trees and reacting to change.
+//! TameTree is a library for synchronising trees and reacting to change.
 //!
-//! ## Philosophy
+//! ## The Why
 //!
-//! Software is hard to grow; TreeSync is designed as a tool to make it easier to build large pieces of software
-//! from smaller ones. This is done by building software as components that use this library for communication.
+//! While 'synchronising trees' is what TameTree does, that doesn't really describe why one might choose to use
+//! it. Here's why: TameTree is a method for building large programs and systems from small programs. Small 
+//! programs are easier to implement and maintain than larger programs, but composing programs is difficult.
 //!
-//! The hypothesis is this: software is easier to work with when it's small: it's easier to reason about and 
-//! easier to test. However, real world requirements pushes software to become ever larger, and this inevitably
-//! results in problems of various kinds. Discipline can stave these problems off but inevitably deadlines and
-//! other pressures causes cracks in the facade as in a conflict between the needs of the software developer and
-//! the needs of a business, the business needs will always end up taking priority.
+//! Modern languages have a lot of composition functionality built in, so it's much more common to do the
+//! composition there. The problem is that this often means components aren't particular isolated from one
+//! another and it's easy to end up with a web of dependencies that can make it very hard to maintain or change
+//! the individual parts.
 //!
-//! If smallness and isolation is inherent to the architecture of a piece of software, then this conflict will
-//! have less effect: small pieces of software are easier to reason about and isolated software can be replaced
-//! at a time that is convenient to both the business and the developer. This seems hard to do when the 
-//! requirements for modern software often involves a large number of interrelated features.
-//! 
-//! TreeSync provides a way to split the design of software into two: a set of components with a narrow, isolated
-//! view of the world, and a system layer that connects the components together into a larger whole. Software is
-//! expanded by writing more components and tying them together at the system layer rather than by growing a single
-//! monolithic whole. Components can be quickly replaced due to their limited dependencies: they don't even need to
-//! be kept small as they can be divided later on if needed.
+//! A TameTree component deals with data in the form of a tree. It monitors an input tree for changes, and it
+//! modifies an output tree in response.
 //!
-//! This is intended to have some parallels with the design of UNIX, which is also designed around small single
-//! purpose components, a feature that has contributed to its longevity by making it possible for it to be 
-//! continuously modernised and adapted to new tasks without requiring a full rewrite.
+//! These trees are abstract: this means it doesn't matter much to a component if its input comes from the same
+//! application or if it is from another process or from a server running over the internet.
 //!
-//! Where UNIX uses streams for communication, TreeSync instead uses trees to better support interactive applications.
+//! This abstraction has one other important advantage: components can be implemented on their own terms without 
+//! needing to take dependencies on other components for APIs or data type definitions. Components can easily
+//! maintain a high level of isolation with respect to each other, while still being able to communicate.
+//!
+//! ## An observation
+//!
+//! Take a typically modern web app. Its gross structure is typically a database, a web server, and a front-end.
+//! The server will talk to a web browser and supply the front-end as HTML, JavaScript and CSS so the browser
+//! can run it. In order to interact with the user, the front-end will then talk back to the server using JSON.
+//! In turn, the server will talk to the database using whatever query language it uses and format the responses
+//! in JSON once again so the front-end can understand them.
+//!
+//! If the app is particularly modern, it will use microservices: multiple webservers that talk amongst themselves
+//! and with the front-end using JSON.
+//!
+//! What's interesting is that all this communication is done with various forms of tree structure: JSON, HTML,
+//! CSS and even the database queries and responses are all types of tree structure. However, code must be written
+//! in order to decode these trees and move data around so that the various parts of the application can communicate.
+//! Complicating things further is that the data being sent is often updates telling the various parts how to
+//! change their state, which can make it hard to determine information about the state of the system as a whole.
+//!
+//! Why is this? The full reason is complicated: the technology stack has aggregated over time to solve a series
+//! of small problems and has come up with different solutions to them, which have been brought together to create
+//! what's recognisable as a modern web app. What's interesting is that there's a common thread: when two isolated
+//! systems - such as the server and the web browser - want to communicate, they do it by determining how they want
+//! the state of the other side to change, encode that change into a tree structure, further encode that structure
+//! into a stream of bytes, send the bytes, decode them back into a tree and then go through and apply the changes.
+//!
+//! The root of all this work is a concept that is now old enough that many have forgotten it had to be invented.
+//! When UNIX was being designed, it was necessary to find a way to connect two processes so they could communicate.
+//! The solution to this was to use streams: the I/O ports of the computers UNIX was running on worked using 
+//! streams of bytes so this would enable processes to talk to each other just like they would talk with the outside
+//! world. The data would be in a textual form so that humans could read them by hooking up a teletype printer and
+//! telling the processes to talk to that instead of each other, which was handy for troubleshooting.
+//!
+//! This decision - use streams for communication - has shaped how the internet works: the socket protocols in
+//! general and HTTP in particular all present themselves as streams of bytes. However, we don't use teletypes
+//! any more, and it turns out programs really want to send more structured data. The end result is that
+//! developers spend a lot of time turning things from streams into trees and back again, and are impressed when
+//! it turns out that JSON makes that process a lot simpler than XML.
+//!
+//! TameTree is a 'what if' project: what if the underlying abstraction processes used to communicate was trees of 
+//! data instead of streams. It adds in a few modern concepts: most notably the idea of being 'reactive' (essentially
+//! watching for changes instead of requesting them)
+//!
+//! The most interesting thing it allows is feedback loops: certainly in the case of our web app. If the HTML is a
+//! tree and we have feedback, then updating the HTML on the server will cause it to update on the client, and events
+//! relating to the HTML displayed on the client will go straight back to the server. That would seem to make quite
+//! lot of that client-side javascript with all of its JSON encoding and decoding obsolete...
 
 extern crate rustc_serialize;
 
