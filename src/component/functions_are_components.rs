052025bac4cde6b4930f0ca934a600b0e168cdee
Andrew Hunter <andrew@logicalshift.co.uk>
2016-03-31 10:57:07 +0100

    Doesn't work: would like to add a variant of ComponentFactory for functions that work only on trees, but rust's typechecker is not smart enough to distinguish different function types

--- src/component/functions_are_components.rs
+++ src/component/functions_are_components.rs
@@ -29,7 +29,11 @@ impl Drop for FunctionComponent {
     }
 }
 
-impl<F: Fn(&TreeChange) -> TreeChange + 'static> ComponentFactory for F {
+///
+/// Simplest form of 'component function': a function that receives a `TreeChange` indicating how the
+/// input tree has changed, and returns a new change indicating how the output has changed.
+///
+impl<F: Fn(&TreeChange) -> TreeChange> ComponentFactory for F {
     ///
     /// Creates a component that consumes from a particular tree and publishes to a different tree
     ///
@@ -41,6 +45,32 @@ impl<F: Fn(&TreeChange) -> TreeChange + 'static> ComponentFactory for F {
         our_consumer.subscribe(TreeAddress::Here, TreeExtent::SubTree, Box::new(move |change| {
             let change_result = action(change);
             our_publisher.publish(change_result);
+        }));
+
+        return Rc::new(FunctionComponent);
+    }
+}
+
+///
+/// Function that receives a tree and returns a modified tree
+///
+impl<F: Fn(&TreeRef) -> TreeRef> ComponentFactory for F {
+    ///
+    /// Creates a component that consumes from a particular tree and publishes to a different tree
+    ///
+    fn create(self, consumer: ConsumerRef, publisher: PublisherRef) -> ComponentRef {
+        let mut our_consumer    = consumer;
+        let mut our_publisher   = publisher;
+        let action              = Box::new(self);
+
+        let mut tree = "empty".to_tree_node();
+
+        our_consumer.subscribe(TreeAddress::Here, TreeExtent::SubTree, Box::new(move |change| {
+            tree = change.apply(tree);
+
+            let new_tree = action(&tree);
+
+            our_publisher.publish(TreeChange::New(&TreeAddress::Here, TreeChangeType::Child, Some(&new_tree)));
         }));
 
         return Rc::new(FunctionComponent);
